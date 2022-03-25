10ee96c04f930dcf797dd17643a84ed0bb801afc
steve.campos <steve.campos@rakuten.com>
2022-03-25 18:38:13 +0100

    add eslint & prettier for react project

--- packages/compose/src/index.tsx
+++ packages/compose/src/index.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-import * as ReactDOM from "react-dom";
-import {Button} from "@corneflex/compose-ui";
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Button } from '@corneflex/compose-ui';
 
-ReactDOM.render(
-    <Button />,
-    document.getElementById('root')
-);
+ReactDOM.render(<Button />, document.getElementById('root'));
