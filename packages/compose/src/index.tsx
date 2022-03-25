aa4318dcff9eb0d3eba8e1d05cab493559adc9e8
steve.campos <steve.campos@rakuten.com>
2022-03-25 17:56:38 +0100

    setup simple react mono repo project with webpack

---
+++ packages/compose/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as ReactDOM from "react-dom";
+import {Button} from "@corneflex/compose-ui";
+
+ReactDOM.render(
+    <Button />,
+    document.getElementById('root')
+);
