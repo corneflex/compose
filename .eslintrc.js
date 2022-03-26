a194f1a2193a592cffee39d2a21113530a6057bf
steve.campos <steve.campos@rakuten.com>
2022-03-26 10:50:10 +0100

    add husky and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'prettier'
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['import', 'react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        project: 'packages/*/tsconfig.json',
+      },
     },
-    plugins: [
-        'react',
-        '@typescript-eslint',
-        'prettier'
-    ],
-    rules: {
-        "prettier/prettier": ["error"]
-    },
+  },
 };
